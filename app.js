70269546cb6e52804aa07d353d287bc8646c11a6
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-07-17 11:58:21 -0700

    Initial server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('Page not found')
+})
+
+app.listen(5500, ()=> {
+    console.log("Server is listening on port 5000......");
+})
