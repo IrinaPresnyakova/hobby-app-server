7531f4ff18a1d4ac68b0f2f890673258146ea1a7
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-07-17 20:49:07 -0700

    Set get request for all projects from data folder

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const hobbies = require ('./data/hobbies.json');
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './'))
+// app.get('/', (req, res)=> {
+//     res.status(200).json(hobbies)
+// })
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/hobby-projects">Details on projects</a>')
 })
+
+app.get('/hobby-projects', (req,res) => {
+    const briefProject = hobbies.map((hobby) => {
+        const { id, name, image } = hobby
+        return { id, name, image }
+    })
+    res.json(briefProject)
+})
+//get a single project using route parameters
+
+app.get('/hobby-projects/:projectID', (req,res) => {
+    // console.log(req.params);
+    const { projectID } = req.params;
+    const singleProject = hobbies.find((hobby) => hobby.id === Number (projectID)) // just === productID if it's a string
+    if (!singleProject) {
+        return res.status(404).send('Project does not exist')
+    }
+
+    res.json(singleProject)
+})
+
 
 app.all('*', (req, res) => {
     res.status(404).send('Page not found')
 })
 
 app.listen(5500, ()=> {
-    console.log("Server is listening on port 5000......");
+    console.log("Server is listening on port 5500......");
 })
