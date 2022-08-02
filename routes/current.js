10e78d7f72c82890acab67c87c6da29c696bc725
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-08-02 10:58:11 -0700

    Resoving merge conflict

--- routes/current.js
+++ routes/current.js
@@ -4,67 +4,27 @@ const { Projects } = require('../models'); //Projects refers to an instance of t
 
 // Current projects page
 
+// Get all projects
 router.get('/', async (req, res) => {
     const listOfProjects = await Projects.findAll(); 
     res.json(listOfProjects); 
 })
+
+// Get project by id
+router.get('/project/:id', async (req, res) => {
+    const id = req.params.id
+    const project = await Projects.findByPk(id) //Projects is the model
+    res.json(project)
+    // if (!singleProject) {
+    //     return res.status(404).send('Project does not exist')
+    // }
+})
+
 
 router.post('/', async (req, res) => {
     const postProject = req.body;
     await Projects.create(postProject);
     res.json(postProject);
 })
-//get a single project using route parameters
-// router.get('/:projectID', (req,res) => {
-//     // console.log(req.params);
-//     const { projectID } = req.params;
-//     const singleProject = currentProjects.find((project) => project.id === Number (projectID)) // just === productID if it's a string
-//     if (!singleProject) {
-//         return res.status(404).send('Project does not exist')
-//     }
-
-//     res.json(singleProject)
-// })
-
-
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-// >>> from the BrainFlix proect, how to post with writing into a file
-// const express = require('express');
-// const router = express.Router();
-// let videos = require ('../data/videos.json');
-// const { v4: uuidv4 } = require('uuid');
-// const fs = require('fs')
-
-// const writeToFile = (targetFile, newUpload ) => {
-//     try {
-//         fs.writeFileSync(targetFile, newUpload)
-//         console.log(`Success`)
-//         return true;
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-// router.post('/', (req, res) => {
-//     const { id, title, description } = req.body;
-
-//     videos.push ({
-//         id: uuidv4(),
-//         title, 
-//         description
-//     });
-
-//     writeToFile('./data/videos.json', JSON.stringify(videos))
-
-//     res.json(videos)
-// })
