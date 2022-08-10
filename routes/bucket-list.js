9df9486aaf0ca3c2e0ed2514d9402be25d85b957
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-08-10 12:55:11 -0700

    Include BL into Projects table in DB

--- routes/bucket-list.js
+++ routes/bucket-list.js
@@ -1,14 +1,50 @@
 const express = require ('express');
 const router = express.Router();
-const { BucketList } = require('../models');
-
-
-//Bucket list page
+const { Projects } = require('../models');
+// Bucket list projects page
+// all projects' titles to show on /archive page
 router.get('/', async (req, res) => {
-    const bucketListOfProjects = await BucketList.findAll(
+    const listOfProjects = await Projects.findAll({
+        where: {'bucketList': 'bucketed'},
+        attributes: [
+            'id',
+            'title',
+        ]}
+    ); 
+    res.json(listOfProjects); 
+})
 
-    );
-    res.json(bucketListOfProjects);    
+router.post('/', async (req, res) => {
+    const postProject = req.body;
+    await Projects.create(postProject);
+    res.json(postProject);
 })
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require ('express');
+// const router = express.Router();
+// const { BucketList } = require('../models');
+
+
+// //Bucket list page
+// router.get('/', async (req, res) => {
+//     const bucketListOfProjects = await BucketList.findAll(
+
+//     );
+//     res.json(bucketListOfProjects);    
+// })
+
+// module.exports = router
