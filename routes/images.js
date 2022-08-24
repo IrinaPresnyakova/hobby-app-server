bb33543b1c837d8a453c72ba552eecd298f5e3c9
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-08-23 18:49:58 -0700

    Process login from cloud and get images

--- routes/images.js
+++ routes/images.js
@@ -1,16 +1,38 @@
 const express = require ('express');
 const router = express.Router();
-const { parsed: config } = dotenv.config();
-const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
+const { json } = require('body-parser')
+require('dotenv').config()
+const axios = require('axios')
+const app = express();
+const cors = require ('cors');
+app.use(json())
+app.use(cors());
+const cloudinary = require("../cloudinary/cloudinary")
 
-const cloudAuth = {
-    cloudUser: config.API_KEY,
-    cloudPassword: config.API_SECRET,
-}
+// const BASE_URL = `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/resources/image`
 
-router.get('/', async (req, res) => {
+// const cloudAuth = {
+//     Username: process.env.API_KEY,
+//     Password: process.env.API_SECRET,
+// }
 
-    res.send('pictures done')
-});
+router.get('/', async (req, res) => {    
+        const {resources} = await cloudinary.search
+            .expression('folder:hobby_app')
+            .execute();
+        const publicIds = resources.map( (file) => file.public_id);
+        res.send(publicIds)
+    })
+
+router.post('/', async(req, res) => {
+    console.log(req.body);
+    const {image} = req.body
+    const uploadedImage = await cloudinary.uploader.upload(image, {
+        upload_preset: "unsigned_upload"
+    })
+    res.json(uploadedImage)
+})
+
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require ('cors');
+const { json } = require('body-parser')
 require('dotenv').config()
 const { PORT } = process.env
-app.use(express.json());
-app.use(cors());
-const db = require('./models')
 
+app.use(express.json({limit: "50mb"}));
+app.use(cors());
+app.use(json())
+const db = require('./models')
+app.use( express.urlencoded({extended: true, limit: '50mb'}))
 
 //Routers
 const currentRouter = require ('./routes/current');
