b1e89d5f36084cb59d2228877790227fc621e6f7
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-09-08 13:49:19 -0700

    Delete image from DB on click

--- routes/images.js
+++ routes/images.js
@@ -14,26 +14,27 @@ const { Images } = require('../models')
 router.get('/:projectId', async (req, res) => {
     const projectId = req.params.projectId
     const imagesBackEnd = await Images.findAll ({
-        attributes: ["public_id"],
+        attributes: ["public_id", "id"],
         where: { 
             ProjectId: projectId,
          }
     })
-    const publicIds = imagesBackEnd.map( (file) => file.public_id);
-    res.send(publicIds)
+    
+    const publicIds = imagesBackEnd.map((file) => {
+        return ({
+            public_id: file.public_id, 
+            id: file.id,   
+            })
+        });
+    res.send(publicIds)//return an object 
+    //console.log(imageDbId); //this prints image ids from DB to console
 });
 
-//get one image???
-router.get('/:projectId/:imageId', async(req, res) => {
-    const id = req.params.id;
-    const image = await Images.findByPk(id)
-    res.json(image)
-})
 
 //For uploading: send images to cloudinary + save data into the database
 router.post('/:projectId', async(req, res) => {
     const ProjectId = req.body.ProjectId;
-    const {image} = req.body
+    const { image } = req.body
     const uploadedImage = await cloudinary.uploader.upload(image, {
         upload_preset: "unsigned_upload"
     })
@@ -46,12 +47,21 @@ router.post('/:projectId', async(req, res) => {
     await newImage.save();
 })
 
+//get one image
+router.get('/:projectId/:id', async(req, res) => {
+    const imageId = req.params.id;
+    const imageData = await Images.findByPk(imageId)
+    res.json(imageData)
+    console.log(imageId);
+})
+
 //For deleting: needs to be 2 things: from DB and from Cloudinary
 router.delete('/:projectId/:id', async (req, res) => {
     const imageId = req.params.id;
     await Images.destroy({
         where: {id: imageId}
     })
+    res.send("Message from backend: This image was deleted")
 })
 
 module.exports = router
